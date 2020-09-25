4054b45396e03dc8335dc66f669a8bb7e13338bc
Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>
2020-09-24 22:19:34 -0300

    Allow receiver to be configured by transmitter

--- receiver/main.cxx
+++ receiver/main.cxx
@@ -17,6 +17,7 @@
 */
 
 #include "../config.h"
+#include "../configpayload.h"
 #include <RF24/RF24.h>
 
 constexpr uint16_t CE_PIN = CONFIG_RECEIVER_CE_PIN;
@@ -27,22 +28,42 @@ int main()
 	RF24 rf24(CE_PIN, CSN_PIN);
 	rf24.begin();
 	rf24.setAddressWidth(CONFIG_ADDRESS_WIDTH);
-	rf24.setRetries(CONFIG_RETRY_DELAY, CONFIG_RETRY_COUNT);
 	rf24.setChannel(CONFIG_CHANNEL);
-	rf24.setPayloadSize(CONFIG_PAYLOAD_SIZE);
 	rf24.setAutoAck(true);
-	rf24.setPALevel(CONFIG_POWER);
-	rf24.setDataRate(CONFIG_BITRATE);
 	rf24.setCRCLength(RF24_CRC_8);
+
+	rf24.setRetries(CONFIG_INITIAL_RETRY_DELAY, CONFIG_INITIAL_RETRY_COUNT);
+	rf24.setPayloadSize(CONFIG_INITIAL_PAYLOAD_SIZE);
+	rf24.setPALevel(CONFIG_INITIAL_POWER);
+	rf24.setDataRate(CONFIG_INITIAL_BITRATE);
+
 	rf24.openReadingPipe(0, CONFIG_ADDRESS);
 	rf24.startListening();
+
+	uint8_t payloadSize;
 
 	while (true)
 	{
 		if (rf24.available())
 		{
-			static uint8_t buffer[CONFIG_PAYLOAD_SIZE];
-			rf24.read(buffer, sizeof(buffer));
+			ConfigPayload config;
+			rf24.read(&config, sizeof config);
+
+			payloadSize = config.payloadSize + 1;
+
+			rf24.setRetries(config.retryDelay, config.retryCount);
+			rf24.setPayloadSize(payloadSize);
+			rf24.setPALevel(config.power);
+			rf24.setDataRate(config.bitrate);
+		}
+	}
+
+	while (true)
+	{
+		if (rf24.available())
+		{
+			static uint8_t buffer[32];
+			rf24.read(buffer, payloadSize);
 		}
 	}
 }
